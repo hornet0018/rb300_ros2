92661ed7b7d4bf1a018f07dda1e4631789ae6f45
kenta-uematsu <38378406+hornet0018@users.noreply.github.com>
2024-08-26 10:58:53 +0900

    Update setup.py

--- ros2_serial_communicator/setup.py
+++ ros2_serial_communicator/setup.py
@@ -2,33 +2,35 @@ from setuptools import setup
 import os
 from glob import glob
 
-package_name = 'ros2_serial_communicator'
+package_name = "ros2_serial_communicator"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
-    package_dir={'': 'src'},  # パッケージが配置されているディレクトリを指定
-    install_requires=['setuptools', 'pyserial'],
+    package_dir={"": "src"},  # パッケージが配置されているディレクトリを指定
+    install_requires=["setuptools", "pyserial", "crc8", "cobs", "serial"],
     zip_safe=True,
-    maintainer='ubuntu',
-    maintainer_email='ubuntu@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="ubuntu",
+    maintainer_email="ubuntu@todo.todo",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'serial_sender = ros2_serial_communicator.serial_communicator:main',
+        "console_scripts": [
+            "serial_sender = ros2_serial_communicator.serial_communicator:main",
         ],
     },
     data_files=[
-        ('share/ament_index/resource_index/packages',
-         ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-        (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
-        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
-        (os.path.join('share', package_name, 'config'), glob('config/*')),
-        (os.path.join('share', package_name, 'configuration_files'), glob('configuration_files/*')),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
+        (os.path.join("share", package_name, "rviz"), glob("rviz/*")),
+        (os.path.join("share", package_name, "urdf"), glob("urdf/*")),
+        (os.path.join("share", package_name, "config"), glob("config/*")),
+        (
+            os.path.join("share", package_name, "configuration_files"),
+            glob("configuration_files/*"),
+        ),
     ],
 )
