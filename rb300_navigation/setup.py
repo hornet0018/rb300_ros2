6127f6bf425dad259ab28a20f63819dd31c29f41
ubuntu <kenta_722@yahoo.co.jp>
2025-05-25 22:02:46 +0900

    update

---
+++ rb300_navigation/setup.py
@@ -0,0 +1,32 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'rb300_navigation'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    # data_files を一つにまとめ、重複を削除
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
+        (os.path.join("share", package_name, "rviz"),    glob("rviz/*")),
+        (os.path.join("share", package_name, "config"),  glob("config/*")),
+        (os.path.join("share", package_name, "map"),     glob("map/*")),
+        (os.path.join("share", package_name, "param"),   glob("param/*.yaml")),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='ubuntu@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
